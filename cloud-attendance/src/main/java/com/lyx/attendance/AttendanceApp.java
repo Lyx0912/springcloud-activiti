da9274bea1fdc490b109fb28522060a658a2b686
lyx <1677685900@qq.com>
2022-11-03 18:21:25 +0800

    考勤模块

---
+++ cloud-attendance/src/main/java/com/lyx/attendance/AttendanceApp.java
@@ -0,0 +1,15 @@
+package com.lyx.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 黎勇炫
+ * @date 2022年11月03日 18:13
+ */
+@SpringBootApplication
+public class AttendanceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceApp.class,args);
+    }
+}
